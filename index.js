c218a49ed44b45419cf992ea679534130027cf2d
NGHSTAR <nghstarcc@gmail.com>
2025-02-15 16:29:28 +0330

    fixing api 1

--- index.js
+++ index.js
@@ -8,12 +8,9 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/persian-amazon/orders", require("./routes/persian-amazon/orders"));
 app.use(
-  "/api/persian-amazon/orders",
-  require("./routes/persian-amazon/orders")
-);
-app.use(
-  "/api/persian-amazon/products",
+  "/persian-amazon/products",
   require("./routes/persian-amazon/products")
 );
 
