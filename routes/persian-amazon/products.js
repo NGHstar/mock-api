d68bf212d09967aa29d24774714aa721ebbb573b
NGHSTAR <nghstarcc@gmail.com>
2025-02-15 16:31:01 +0330

    fix get products api

--- routes/persian-amazon/products.js
+++ routes/persian-amazon/products.js
@@ -3,16 +3,12 @@ const fs = require("fs");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  fs.readFile(
-    "routes/persian-amazon/data/products.json",
-    "utf8",
-    (err, data) => {
-      if (err) {
-        return res.status(500).json({ error: "مشکل در خواندن فایل محصولات" });
-      }
-      res.json(JSON.parse(data));
+  fs.readFile("./data/products.json", "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "مشکل در خواندن فایل محصولات" });
     }
-  );
+    res.json(JSON.parse(data));
+  });
 });
 
 module.exports = router;
