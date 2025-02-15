31c57c1b9aa7609776c2984a1fe0b6a48410e37f
NGHSTAR <nghstarcc@gmail.com>
2025-02-15 16:48:22 +0330

    fix products 4

--- routes/persian-amazon/products.js
+++ routes/persian-amazon/products.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  fs.readFile(
-    "routes/persian-amazon/data/products.json",
-    "utf8",
-    (err, data) => {
-      if (err) {
-        return res.status(500).json({ error: err.message });
-      }
-      res.json(JSON.parse(data));
+  const filePath = path.join(__dirname, "data", "products.json"); // Corrected path
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
     }
-  );
+    res.json(JSON.parse(data));
+  });
 });
 
 module.exports = router;
